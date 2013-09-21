8d6c6c738505e19d10b3b6a0a45b2f9bb6a7cd03
seanwooj <seanwoojinkim@gmail.com>
2013-09-21 10:03:25 -0700

    initialized rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MakeIt3d::Application.config.secret_key_base = '74172bffc45c735b231ceff7a0bfbaa0192abfd7ff64c3c16240d2c7f987d61a66fd105235ecd799222f0e9e1d621e1f44a3ca423e9e755641b85a47f248969b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MakeIt3d::Application.config.session_store :cookie_store, key: '_make-it-3d_session'
